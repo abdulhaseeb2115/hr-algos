3f62b535478d51532a0c63a9d245508d060b9450
abhaseeb2115 <abhaseeb2115@gmail.com>
2024-01-10 00:52:38 +0500

    added sparse array function

---
+++ sparseArrays.js
@@ -0,0 +1,24 @@
+/**
+ * There is a collection of input strings and a collection of query strings.
+ * For each query string, determine how many times it occurs in the list of input strings.
+ * Return an array of the results.
+ *
+ *
+ * @param {String[]} strings
+ * @param {String[]} queries
+ * @returns {number[]}
+ *
+ *
+ * Sample Input:
+ * strings = ["aba", "baba", "aba", "xzxb"]
+ * queries = ["aba", "xzxb", "ab"];
+ * Sample Output:
+ * [2, 1, 0]
+ */
+const matchingStrings = (strings, queries) => {
+	let stringsFrequencyMap = {};
+	strings.forEach((str) => {
+		stringsFrequencyMap[str] = (stringsFrequencyMap[str] || 0) + 1;
+	});
+	return queries.map((query) => stringsFrequencyMap[query] || 0);
+};
